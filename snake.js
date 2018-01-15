2f045403b3ce2ae723e2cda672937be72ea78075
edforsberg <31727750+edforsberg@users.noreply.github.com>
2018-01-15 21:06:30 +0000

    started creationg snake

---
+++ snake.js
@@ -0,0 +1,13 @@
+
+
+class snake{
+
+	var dirrEnum = {"up":1, "down":2, "left":3, "down":4};
+	
+	var length = 4; //int length of snake
+	var speed = 15; //int snek sped
+	var currDir = dirrEnum.up; //current snake direction
+
+	
+
+}
