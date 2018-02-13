ab1225c101c5ad53e93d9719349dd1e7d9f24b8b
edforsberg <31727750+edforsberg@users.noreply.github.com>
2018-02-13 19:31:25 +0000

    added UID class and greated move method

--- snake.js
+++ snake.js
@@ -1,13 +1,33 @@
 
 
-class snake{
+class snake
+{
 
-	var dirrEnum = {"up":1, "down":2, "left":3, "down":4};
+	var dirrEnum = {"up":1, "down":2, "left":3, "right":4};
 	
-	var length = 4; //int length of snake
+	var path = []; //all coordinates occupied by the snake
 	var speed = 15; //int snek sped
-	var currDir = dirrEnum.up; //current snake direction
+	var direction = dirrEnum.up; //current snake direction
+	var instanceID = generateUID(); 
+	var isAlive = true;
+	var thickness = 1;
 
-	
+
+
+	function changeDir(){}
+
+	function move(direction, speed, path)
+	{
+
+var currDir = changeDir();
+var mouthPosition = path[0];
+var newMouthPos = 
+
+if (path == dirrEnum.up) {}
+	else if (path == dirrEnum.down){}
+		else if (path == dirrEnum.right){}
+			else {}
+
+	}
 
 }
