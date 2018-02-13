834b4fd2209b439401d4e030ae83d75a7b5c03e1
edforsberg <31727750+edforsberg@users.noreply.github.com>
2018-02-13 19:51:20 +0000

    completed first version of move function
    in the snake class

--- snake.js
+++ snake.js
@@ -5,7 +5,21 @@ class snake
 
 	var dirrEnum = {"up":1, "down":2, "left":3, "right":4};
 	
-	var path = []; //all coordinates occupied by the snake
+	var path = [
+		{
+			x : 50,
+			y : 50
+		},
+		{
+			x : 51, 
+			y : 50
+		},
+		{
+			x : 52, 
+			y : 50
+		}
+
+	]; //all coordinates occupied by the snake
 	var speed = 15; //int snek sped
 	var direction = dirrEnum.up; //current snake direction
 	var instanceID = generateUID(); 
@@ -16,18 +30,24 @@ class snake
 
 	function changeDir(){}
 
-	function move(direction, speed, path)
+	function move(snakeFood)
 	{
 
+var foodPos = snakeFood.position;
 var currDir = changeDir();
 var mouthPosition = path[0];
-var newMouthPos = 
+var newMouthPos = [];
 
-if (path == dirrEnum.up) {}
-	else if (path == dirrEnum.down){}
-		else if (path == dirrEnum.right){}
-			else {}
+if (path == dirrEnum.up) {newMouthPos = {mouthPosition[0], mouthPosition[1]+1}}
+	else if (path == dirrEnum.down){newMouthPos = {mouthPosition[0], mouthPosition[1]-1}}
+		else if (path == dirrEnum.right){newMouthPos = {mouthPosition[0]+1, mouthPosition[1]}}
+			else {newMouthPos = {mouthPosition[0]-1, mouthPosition[1]}}
 
+var newPath = [newMouthPos];
+newPath.Push(path[_.range(0, path.length-2)]);
+if (foodPos == mouthPosition) {newPath.Push(path[path.length-1])}
+snake.path = newPath;
 	}
+}
 
 }
