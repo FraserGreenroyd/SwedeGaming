906d9298d3647c2dad679e069a0bf6cd7c669707
Sofia Malmsten <sofia.malmsten@burohappold.com>
2018-03-12 20:40:43 +0000

    Added test data

--- database/UserData.sql
+++ database/UserData.sql
@@ -1,3 +1,4 @@
 INSERT INTO User (Username, Email, EmailVerified, DateCreated, DoB,	FirstName, LastName, Password, PassHash, DateUpdated, ImageID, Active, AdminUserID)
 VALUES ('Tibbelina','hello@selmabjorklund.com','1',CURRENT_TIMESTAMP,CURRENT_date,'Selma','Bjorklund','selmaspass','ENCHASH',CURRENT_TIMESTAMP,'123','1','1');
-VALUES ('SelmasUser','selmasemail@gmail.com','0',NOW(),current_date,'Maria', 'Karlsson','nyttpass','lasmig',NOW(),'563','1','1')
+VALUES ('SelmasUser','selmasemail@gmail.com','0',NOW(),current_date,'Maria', 'Karlsson','nyttpass','lasmig',NOW(),'563','1','1');
+VALUES ('Sofmal','sofia.malmsten@me.com','1',CURRENT_TIMESTAMP,CURRENT_date,'Sofia','Malmsten','soffispass','ENCHASH',CURRENT_TIMESTAMP,'1','1')
