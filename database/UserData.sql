f3259f7c723edf99306c11565738137b4fcb461b
Selma Björklund <hello@selmabjorklund.com>
2018-03-12 20:37:26 +0000

    Remove Admin ID from dummy data

--- database/UserData.sql
+++ database/UserData.sql
@@ -1,3 +1,3 @@
-INSERT INTO User (Username, Email, EmailVerified, DateCreated, DoB,	FirstName, LastName, Password, PassHash, DateUpdated, ImageID, Active, AdminUserID)
+INSERT INTO User (Username, Email, EmailVerified, DateCreated, DoB,	FirstName, LastName, Password, PassHash, DateUpdated, ImageID, Active)
 VALUES ('Tibbelina','hello@selmabjorklund.com','1',CURRENT_TIMESTAMP,CURRENT_date,'Selma','Bjorklund','selmaspass','ENCHASH',CURRENT_TIMESTAMP,'123','1');
 VALUES ('SelmasUser','selmasemail@gmail.com','0',NOW(),current_date,'Maria', 'Karlsson','nyttpass','lasmig',NOW(),'563','1')
