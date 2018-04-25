1df73e85b61df41111126c89b2a3fc872a50c95f
Erica Samuelsson <EricaSamuelsson@Ericas-MacBook-Pro.local>
2018-04-25 19:16:18 +0100

    Pushing some Frontend changes

--- public/frontend/views/index.php
+++ public/frontend/views/index.php
@@ -1,6 +1,9 @@
 <?php include('../layout/header.html'); ?>
 
-<div class="ng-hide" ng-show="isLoading"><span class="fa fa-spinner fa-pulse fa-5x fa-fw"></span></div>
+<div class="ng-hide" ng-show="isLoading">
+	<span class="fa fa-spinner fa-pulse fa-5x fa-fw">
+	</span>
+</div>
 
 <ng-include src="currentPage"></ng-include>
 
